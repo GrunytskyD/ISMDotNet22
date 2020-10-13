4d7284ad9d61292904aa263ca7de0b7eff189ab6
Dmytro Grunytsky <dima.grunitsky@gmail.com>
2020-10-13 21:29:58 +0300

    Значення у

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Значення x");
+            double numberx = double.Parse(Console.ReadLine());
+            Console.WriteLine("Значення a");
+            double numbera = double.Parse(Console.ReadLine());
+            Console.WriteLine("Значення b");
+            double numberb = double.Parse(Console.ReadLine());
+            double y=numberx*numbera/numberb;
+            Console.WriteLine($"у={y}");
+        }
+    }
+}
